62778d80a63b0950fce379ad0cc0c82cbba42fc0
pphius <pphius@gmail.com>
2026-02-25 23:46:51 +0200

    stage printf push2

--- newmain.c
+++ newmain.c
@@ -21,7 +21,7 @@ int main(int argc, char** argv) {
     int num1;
     int num2;
     int sum;
-    printf("Enter num0 and num2\n");
+    printf("Enter num1 and num2\n");
     scanf("%d %d",&num1, &num2);
 
    sum = num1 + num2;
