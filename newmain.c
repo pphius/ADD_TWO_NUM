d7aea6570b42988836f2621a901168e78bf453a2
pphius <pphius@gmail.com>
2026-02-25 21:41:30 +0200

    add two num repo

---
+++ newmain.c
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   newmain.c
+ * Author: phiuspetrus
+ *
+ * Created on August 23, 2025, 3:05 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int num1;
+    int num2;
+    int sum;
+    printf("Enter num1 and num2\n");
+    scanf("%d %d",&num1, &num2);
+
+   sum = num1 + num2;
+    printf("Sum of %d and %d is %d", num1, num2,
+           sum);
+    
+    return (EXIT_SUCCESS);
+}
+
